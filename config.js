ebfdfde48f08ead2e54580d02fd8affd0333235a
craftaurabypooja <craftaurabypooja@gmail.com>
2026-09-10 23:02:27 +0530

    Update and rename config.js to js/config.js

--- config.js
+++
@@ -1,9 +0,0 @@
-// Paste your deployed Google Apps Script Web App URL below.
-// Example: https://script.google.com/macros/s/XXXXXXXXXXXX/exec
-const CONFIG = {
-  API_URL: "PASTE_YOUR_GOOGLE_APPS_SCRIPT_WEB_APP_URL_HERE",
-  STORE_NAME: "CraftAura by Pooja",
-  PHONE: "8517935887",
-  ADDRESS: "10, Parihar Market, Tejaji Nagar, Khandwa Road, Indore 452020",
-  CURRENCY: "₹"
-};
---
+++ js/config.js
@@ -0,0 +1,9 @@
+// Paste your deployed Google Apps Script Web App URL below.
+// Example: https://script.google.com/macros/s/AKfycbyQA9D3xrMeDhs8MctkPOXOhQD-VczVtx21p26k48YxEF_GU0VRiiG2H6tecljYzvsO4Q/exec
+const CONFIG = {
+  API_URL: "https://script.google.com/macros/s/AKfycbyQA9D3xrMeDhs8MctkPOXOhQD-VczVtx21p26k48YxEF_GU0VRiiG2H6tecljYzvsO4Q/exec",
+  STORE_NAME: "CraftAura by Pooja",
+  PHONE: "8517935887",
+  ADDRESS: "10, Parihar Market, Tejaji Nagar, Khandwa Road, Indore 452020",
+  CURRENCY: "₹"
+};
